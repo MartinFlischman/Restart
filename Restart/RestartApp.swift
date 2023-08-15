139884ba25680aa3af744aebb511fc78879b33a4
Martin Flischman <wipes_meshes.03@icloud.com>
2023-08-15 18:14:36 +0200

    Initial Commit

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Martin Flischman on 2023/08/15.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
