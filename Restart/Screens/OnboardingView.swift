70d9c4b500f7b959a64020b923b41a3d789aef44
Martin Flischman <martincodes@icloud.com>
2024-10-10 19:50:07 +0200

    Added haptic feedback and audio play functionality
    feat: add haptic feedback generator to onboarding button for success and warning feedback
    
    feat: play sound upon successful onboarding completion using AVAudioPlayer for audio feedback
    
    chore: created new AudioPlayer.swift file to handle sound playback functionality in the app
    
    fix: adjusted buttonOffset logic to include haptic and sound triggers when user completes drag interaction

--- Restart/Screens/OnboardingView.swift
+++ Restart/Screens/OnboardingView.swift
@@ -18,6 +18,8 @@ struct OnboardingView: View {
     @State private var indicatorOpacity: Double = 1.0
     @State private var textTitle: String = "Share."
     
+    let hapticFeedback = UINotificationFeedbackGenerator()
+    
     // MARK: - Body
     var body: some View {
         ZStack {
@@ -147,9 +149,12 @@ struct OnboardingView: View {
                                 }
                                 .onEnded { _ in
                                     if buttonOffset > buttonWidth / 2 {
+                                        hapticFeedback.notificationOccurred(.success)
+                                        playSound(sound: "chimeup", type: "mp3")
                                         buttonOffset = buttonWidth - 80
                                         isOnboardingViewActive = false
                                     } else {
+                                        hapticFeedback.notificationOccurred(.warning)
                                         buttonOffset = 0
                                     }
                                 }
@@ -169,6 +174,7 @@ struct OnboardingView: View {
         .onAppear(perform: {
             isAnimated = true
         })
+        .preferredColorScheme(.dark)
     }
 }
 
